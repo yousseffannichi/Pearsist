5c9508930150d3b794fd48afa2c47d52216d6242
yousseffannichi <youssef.fannichi001@umb.edu>
2021-04-25 17:49:16 -0400

    Updated about

--- AutopushGit.bat
+++ AutopushGit.bat
@@ -1,7 +1,7 @@
 cd C:\Users\youss\OneDrive\Desktop\Dehack Project\Pearsist
 git checkout master
 git add ./*
-git commit -am "made changes"
+git commit -am "Updated about"
 git push
 echo Press Enter...
 read
