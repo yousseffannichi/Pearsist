85131df2aefb3a473b3b53f437bf654e80c7a4ac
yousseffannichi <youssef.fannichi001@umb.edu>
2021-04-25 17:48:00 -0400

    made changes

--- AutopushGit.bat
+++ AutopushGit.bat
@@ -1,12 +1,7 @@
 cd C:\Users\youss\OneDrive\Desktop\Dehack Project\Pearsist
-# switch to branch you want to use
 git checkout master
-# add all added/modified files
 git add ./*
-# commit changes
 git commit -am "made changes"
-# push to git remote repository
 git push
-###########################
 echo Press Enter...
 read
